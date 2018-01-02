e6785a6ea7f4a9baed002a5de15f780866d27d8f
rakesh-kumar <rakeshmaurya262@gmail.com>
2018-01-02 14:14:13 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180102062940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dependencies", force: :cascade do |t|
     t.string "name"
     t.integer "task_id"
